225f7dac1c5f0c0814d3ffbffde5124991dab6ed
Paul Shippy <paul.shippy@strongmind.com>
2023-04-24 10:00:27 -0700

    Try passing container image

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -1,5 +1,9 @@
+import os
 import pulumi
 from strongmind_deployment.rails import RailsComponent
 
 stack_name = pulumi.get_stack()
-component = RailsComponent(stack_name, container_port=80)
+container_image = os.environ['CONTAINER_IMAGE']
+component = RailsComponent(stack_name,
+                           container_port=80,
+                           container_image=container_image)
