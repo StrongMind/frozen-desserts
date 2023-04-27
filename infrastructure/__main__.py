0d2084e9f746e5f65fa0ecfa40a0f179df2ed36a
Paul Shippy <paul.shippy@strongmind.com>
2023-04-27 16:40:17 -0700

    Pass master key

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -2,8 +2,13 @@ import os
 import pulumi
 from strongmind_deployment.rails import RailsComponent
 
+master_key = os.environ['RAILS_MASTER_KEY']
+
 stack_name = pulumi.get_stack()
 container_image = os.environ['CONTAINER_IMAGE']
 component = RailsComponent(stack_name,
                            container_port=3000,
-                           container_image=container_image)
+                           container_image=container_image,
+                           env_vars={
+                               'RAILS_MASTER_KEY': master_key,
+                           })
